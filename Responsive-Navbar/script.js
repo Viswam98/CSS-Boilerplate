dedc523a503cef89bfe3405785c02786735c5e68
Abhiram Viswam <abhiramviswam1998@gmail.com>
2022-08-13 22:33:52 +0530

    added hamburger menu to X animation

--- Responsive-Navbar/script.js
+++ Responsive-Navbar/script.js
@@ -1,8 +1,12 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+const bars = document.querySelectorAll('.bar')
+
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     toggleButton.classList.toggle('rotate')
+
+    bars.forEach(bar => bar.classList.toggle('x'))
 })
 
